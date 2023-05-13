646adf8540dda2b99bd65ed22bdb332aac35c07f
unknown <saran332003@gmail.com>
2023-05-13 12:43:27 +0530

    last

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Imagge from 'next/image'
+
+export default function Home() {
+  return (
+    <main className=" bg-white ">
+      <h2 className=' bg-red-600 text-white'> Hello world </h2>
+    </main>
+  )
+}
